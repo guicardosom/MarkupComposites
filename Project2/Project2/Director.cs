de0c59d0d38968df6b596cdda7b6eda47f69bce8
gccmiranda <gccardosomiranda@outlook.com>
2023-07-04 19:41:13 -0400

    Starts IDirector implementation

---
+++ Project2/Project2/Director.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Project2
+{
+	public class Director : IDirector
+	{
+        private IBuilder builder;
+
+		public Director(IBuilder b)
+		{
+            builder = b;
+        }
+
+        public void BuildBranch()
+        {
+            builder.BuildBranch();
+        }
+
+        public void BuildLeaf()
+        {
+            builder.BuildLeaf();
+        }
+
+        public void CloseBranch()
+        {
+            builder.CloseBranch();
+        }
+    }
+}
+
