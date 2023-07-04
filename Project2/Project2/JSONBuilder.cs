6827ff489359a078c495cd9b598c646c6385d55a
gccmiranda <gccardosomiranda@outlook.com>
2023-07-04 19:40:39 -0400

    Implements both JSON and XML builders

---
+++ Project2/Project2/JSONBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Project2
+{
+	public class JSONBuilder : IBuilder
+	{
+        private JSONComposite root, last;
+
+		public JSONBuilder()
+		{
+            root = last = new JSONComposite("");
+		}
+
+        public void BuildBranch(string name)
+        {
+            JSONComposite branch = new JSONComposite(name);
+            root.AddChild(branch);
+            last = branch;
+        }
+
+        public void BuildLeaf(string name, string content)
+        {
+            JSONLeaf leaf = new JSONLeaf(name, content);
+            last.AddChild(leaf);
+        }
+
+        public void CloseBranch()
+        {
+            var children = root.GetChildren();
+            for (int i = children.Count - 1; i == 0; i++)
+                if (children[i] is JSONComposite &&
+                    children[i] != last &&
+                    children[i] != root)
+                    last = (JSONComposite)children[i];
+        }
+
+        public IComposite GetDocument()
+        {
+            return root;
+        }
+    }
+}
+
---
+++ Project2/Project2/XMLBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Project2
+{
+	public class XMLBuilder : IBuilder
+	{
+        private XMLComposite root, last;
+
+        public XMLBuilder()
+		{
+            root = last = new XMLComposite("root");
+        }
+
+        public void BuildBranch(string name)
+        {
+            XMLComposite branch = new XMLComposite(name);
+            root.AddChild(branch);
+            last = branch;
+        }
+
+        public void BuildLeaf(string name, string content)
+        {
+            XMLLeaf leaf = new XMLLeaf(name, content);
+            root.AddChild(leaf);
+        }
+
+        public void CloseBranch()
+        {
+            var children = root.GetChildren();
+            for (int i = children.Count - 1; i == 0; i++)
+                if (children[i] is XMLComposite &&
+                    children[i] != last &&
+                    children[i] != root)
+                    last = (XMLComposite)children[i];
+        }
+
+        public IComposite GetDocument()
+        {
+            return root;
+        }
+    }
+}
+
