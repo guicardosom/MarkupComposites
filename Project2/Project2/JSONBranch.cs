7d3281614c495ae4a77770024766646e7a824e74
gccmiranda <gccardosomiranda@outlook.com>
2023-07-06 14:45:10 -0400

    Updates JSON and XML branches to use depth when printing

---
+++ Project2/Project2/JSONBranch.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Project2
+{
+	public class JSONBranch : IComposite
+	{
+        private List<IComposite> _children;
+        private string name;
+
+        public JSONBranch(string n)
+        {
+            name = n;
+            _children = new List<IComposite>();
+        }
+
+        public void AddChild(IComposite child)
+        {
+            if (!_children.Contains(child))
+                _children.Add(child);
+        }
+
+        public string Print(int depth)
+        {
+            string jsonFormattedText = "";
+
+            if (!string.IsNullOrEmpty(name))
+                jsonFormattedText += String.Format("{0," + depth * 4 + "}\'{1,0}\':\n", " ", name);
+
+            jsonFormattedText += String.Format("{0," + depth * 4 + "}{1,0}\n", " ", '{');
+
+            foreach (var child in _children)
+            {
+                jsonFormattedText += child.Print(depth + 1);
+
+                if (child != _children.Last())
+                    jsonFormattedText += ',';
+
+                jsonFormattedText += '\n';
+            }
+
+            jsonFormattedText += String.Format("{0," + depth * 4 + "}{1,0}", " ", "}");
+
+            return jsonFormattedText;
+        }
+    }
+}
+
---
+++ Project2/Project2/XMLBranch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Project2
+{
+	public class XMLBranch : IComposite
+	{
+        private List<IComposite> _children;
+        private string name;
+
+        public XMLBranch(string n)
+        {
+            name = n;
+            _children = new List<IComposite>();
+        }
+
+        public void AddChild(IComposite child)
+        {
+            if (!_children.Contains(child))
+                _children.Add(child);
+        }
+
+        public string Print(int depth)
+        {
+            string xmlFormattedText = String.Format("{0," + depth * 4 + "}<{1,0}>\n", " ", name);
+
+            foreach (var child in _children)
+            {
+                xmlFormattedText += child.Print(depth + 1);
+                xmlFormattedText += '\n';
+            }
+
+            xmlFormattedText += String.Format("{0," + depth * 4 + "}</{1,0}>", " ", name);
+
+            return xmlFormattedText;
+        }
+    }
+}
+
